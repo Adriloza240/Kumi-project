8c3c0717b951e264fc85b4807ca4b265f444388d
Adriloza240 <152127693+Adriloza240@users.noreply.github.com>
2025-06-07 18:39:04 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Kumi-project/',
+  server: {
+    proxy: {
+      // Para desarrollo local
+      '/db.json': {
+        target: 'http://localhost:5173',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/db.json/, '/db.json')
+      }
+    }
+  }
+});
